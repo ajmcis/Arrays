f40d90f2893f06b339f092ea355a4a31f85bb280
ajmcis <macka239@yahoo.com>
2015-07-27 03:35:16 -0400

    adding Arrays.cpp

---
+++ Arrays.cpp
@@ -0,0 +1,51 @@
+//============================================================================
+// Name        : Arrays.cpp
+// Author      : Alex Mack
+// Version     :
+// Copyright   : (c) 2015 Mack
+// Description : Doubles the integer values stored in an array then prints them out
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void PrintArray(int [], int);
+void DoubleIt(int [], int);
+
+int main() {
+	const int SIZE = 5;
+	int num[SIZE];
+
+	for (int i = 0; i < SIZE; i++) {
+		 cout << "Enter value #" << i + 1 << ": ";
+		 cin >> num[i];
+	}
+
+	cout << "\nArray: ";
+	PrintArray(num, SIZE);
+
+	cout <<"Array with double the original values: ";
+	DoubleIt(num, SIZE);
+	PrintArray(num, SIZE);
+
+	return 0;
+}
+
+void PrintArray(int num [], int size){
+
+	for (int i = 0; i < size; i++) {
+		 cout << num[i] << ", ";
+
+	}
+
+	cout << endl;
+
+}
+
+void DoubleIt(int num [], int size){
+
+	for (int i = 0; i < size; i++) {
+		 num[i] *= 2;
+
+	}
+}
